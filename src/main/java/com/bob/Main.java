fe306687e7e5d70487c6f4539a1886a47df454e9
dobob09 <dobob@qq.com>
2018-03-10 21:50:51 +0800

    add file

---
+++ src/main/java/com/bob/Main.java
@@ -0,0 +1,34 @@
+package com.bob;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * 程序入口，负责启动应用
+ * @author dobob09
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            bootstrap.group(bossGroup, workGroup);
+            bootstrap.channel(NioServerSocketChannel.class);
+            bootstrap.childHandler(new MyWebSocketChannelHandler());
+            System.out.println("服务端开启等待客户端连接。。。");
+            Channel channel = bootstrap.bind(8888).sync().channel();
+            channel.closeFuture().sync();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            //优雅的退出程序
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/bob/NettyConfig.java
@@ -0,0 +1,17 @@
+package com.bob;
+
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * 存储整个工程的全局配置
+ *
+ * @author dobob09
+ */
+public class NettyConfig {
+    /**
+     * 存储每个客户端接入进来时的channel对象
+     */
+    public static ChannelGroup group = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+}
